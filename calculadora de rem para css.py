75c471ecdd8f9100a4ab7fb786888fbfd1f6d590
SENSEI620 <81596161+SENSEI620@users.noreply.github.com>
2024-05-16 15:50:26 -0300

    Add files via upload

---
+++ calculadora de rem para css.py
@@ -0,0 +1,12 @@
+
+
+while True:
+    bodyPx = 16 #16 px
+    px = int(input("valor de px: "))
+
+    rem = px / bodyPx   
+
+    print(f" o valor para ser usado em REM = {rem:.2f}")
+    
+    if px == str:
+        break
